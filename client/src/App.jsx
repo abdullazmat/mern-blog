8b7f3b03769f372f2a3a7d0c7b57faaa0e2de664
Abdullah <shinsukenakamurahard@gmail.com>
2025-10-27 23:31:39 +0500

    Pages and Routes Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Projects from "./pages/Projects"
+
+
+
 function App() {
 
   return (
-  <div className="text-red-700">
-    Hello
-  </div>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element = {<Home /> } />
+  <Route path="/about" element = {<About /> } />
+  <Route path="/signup" element = {<SignUp /> } />
+  <Route path="/signin" element = {<SignIn /> } />
+  <Route path="/projects" element = {<Projects /> } />
+</Routes>
+
+</BrowserRouter>
   )
 }
 
