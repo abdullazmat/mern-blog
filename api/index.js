94e0e49db7f16f548b54fa268a0f378606c2308d
Abdullah <shinsukenakamurahard@gmail.com>
2025-11-09 04:20:52 +0500

    mongodb cluster setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("API server is running on http://localhost:3000");
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
