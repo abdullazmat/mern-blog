9e6c01e74921845f517cf19a45bb2493eb557a7c
Abdullah <shinsukenakamurahard@gmail.com>
2025-11-09 04:39:22 +0500

    user router and controller setup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("API server is running on http://localhost:3000");
@@ -18,3 +21,5 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
+
+export default app;
