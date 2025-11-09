a6c357dcff34ecd9e4ff8e55218467b672c31fca
Abdullah <shinsukenakamurahard@gmail.com>
2025-11-09 06:02:10 +0500

    Sign up api route setup

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.models.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully", newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("API server is running on http://localhost:3000");
